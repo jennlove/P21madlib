a473c5ef59bd054e7dd2529738fbbce18fe6b4cb
alleydragon <jenniferlove@comcast.net>
2019-02-28 11:26:21 -0500

    Firt commit: If/then approach

---
+++ P21.py
@@ -0,0 +1,33 @@
+print ("It Could Have Happened That Way, Birth)'\n")
+
+def get_word(part_of_speech):
+    if (part_of_speech == 'noun'):
+        return input("Enter a noun: ")
+    elif (part_of_speech == 'verb'):
+        return input("Enter a verb: ")
+    elif (part_of_speech == 'ing_verb'):
+        return input("Enter a verb (ing form): ")
+    elif (part_of_speech == 'adj'):
+        return input("Enter an adjective: ")
+    elif (part_of_speech == 'name'):
+        return input("Enter a person's name: ")
+    elif (part_of_speech == 'day'):
+        return input("Enter a day: ")
+
+print("I remember when Patrick was born like it was " + get_word('day') + \
+      ". It was still number days before he was due but I was READY.  "
+      "I weighed number pounds and looked like I'd swallowed a noun \
+      Bruce was ready, too.  He'd had enough of my verbing all the time and "
+      "verbing nouns and was tired of running out to store at a moment's notice to "
+      "buy 'Food'.  So, I was verbing on the noun and then decided to go to the
+      'room'.
+
+      him to run out to Store to get me Pnoun.
+      I felt "adj and was just lying on the noun"
+      "I was lying on the noun looking like a noun \
+      with a belly the size of a noun.   Exclamation, I said as I started to feel
+      the first adjective contraction.  I think today's the day.  '
+      "We had just finished " + get_word('adj') + \
+      " and Dad was ing_verb the noun. \
+      " I It's " + get_word('name') + ". "
+      "You'd better not " + get_word('verb') + "!")
